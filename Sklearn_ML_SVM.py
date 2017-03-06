8e2bb5814eb0c3e4c27c3378d5f7a54c5426d29e
SimonBison <simonpeterbison@gmail.com>
2017-03-06 20:40:17 +0100

    checked the length of the data

--- Sklearn_ML_SVM.py
+++ Sklearn_ML_SVM.py
@@ -5,7 +5,8 @@ from sklearn import svm
 
 digits = datasets.load_digits()
 
-print(digits.data)
-print(digits.target)
-print(digits.images[0])
+clf = svm.SVC(gamma=0.001, C=100)
 
+print(len(digits.data))
+
+x, y = digits.data[:-1], digits.target[:-1]
