fd4e4e6bfb51e1fe13c6572d6e79b14e0c32a6a5
SimonBison <simonpeterbison@gmail.com>
2017-03-07 14:16:17 +0100

    final version that predicts the digit(s)

--- Sklearn_ML_SVM.py
+++ Sklearn_ML_SVM.py
@@ -1,3 +1,5 @@
+# Predicting digits with SVM
+
 import matplotlib.pyplot as plt
 
 from sklearn import datasets
@@ -7,6 +9,15 @@ digits = datasets.load_digits()
 
 clf = svm.SVC(gamma=0.001, C=100)
 
+# shows how many we have examples of digits
 print(len(digits.data))
 
-x, y = digits.data[:-1], digits.target[:-1]
+# storing all of the answers
+x, y = digits.data[:-10], digits.target[:-10]
+clf.fit(x, y)
+
+# predict what is the negative first element
+print('Prediction:', clf.predict(digits.data[-3]))
+
+plt.imshow(digits.images[-3], cmap=plt.cm.gray_r, interpolation="nearest")
+plt.show()
